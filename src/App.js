ce08e0664c6004ad2666c2f8597330d701e5af9f
Royxuzeng <e0376917@u.nus.edu>
2023-03-29 21:20:02 +0800

    add course

--- src/App.js
+++ src/App.js
@@ -1,21 +1,56 @@
-import Header from './Header.js'
-import Content from './Content.js'
-import Total from './Total.js'
+import Course from './Course'
 
 const App = () => {
-  // const-definitions
-  const course = 'Half Stack application development'
-  const parts = [
-    { name: 'Fundamentals of React', exercises: 10 },
-    { name: 'Using props to pass data', exercises: 7 },
-    { name: 'State of a component', exercises: 14 },
-  ];
+  const courses = [
+    {
+      name: 'Half Stack application development',
+      id: 1,
+      parts: [
+        {
+          name: 'Fundamentals of React',
+          exercises: 10,
+          id: 1
+        },
+        {
+          name: 'Using props to pass data',
+          exercises: 7,
+          id: 2
+        },
+        {
+          name: 'State of a component',
+          exercises: 14,
+          id: 3
+        },
+        {
+          name: 'Redux',
+          exercises: 11,
+          id: 4
+        }
+      ]
+    },
+    {
+      name: 'Node.js',
+      id: 2,
+      parts: [
+        {
+          name: 'Routing',
+          exercises: 3,
+          id: 1
+        },
+        {
+          name: 'Middlewares',
+          exercises: 7,
+          id: 2
+        }
+      ]
+    }
+  ]
 
   return (
     <div>
-      <Header course={course} />
-      <Content parts={parts} />
-      <Total parts={parts} />
+      {courses.map(course => (
+        <Course key={course.id} course={course} />
+      ))}
     </div>
   )
 }
---
+++ src/Course.js
@@ -0,0 +1,16 @@
+import Header from './Header.js'
+import Content from './Content.js'
+import Total from './Total.js'
+
+
+const Course = ({ course }) => {
+    return (
+        <div>
+          <Header courseName={course.name} />
+          <Content parts={course.parts} />
+          <Total parts={course.parts} />
+        </div>
+      )
+}
+
+export default Course
